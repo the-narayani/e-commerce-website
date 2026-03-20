06189149f539b84a909e828baa09b641fdab795b
the-narayani <narayanikk15@gmail.com>
2026-03-20 18:40:17 -0400

    rendered all components in App.jsx

--- ecommerce-app/src/App.jsx
+++ ecommerce-app/src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import ProductDetails from "./components/ProductDetails";
+import Cart from "./components/Cart";
+import Wishlist from "./components/Wishlist";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
