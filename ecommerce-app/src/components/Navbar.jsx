addcc626e0c6cfd60ab303309313dfe9bf44f964
the-narayani <narayanikk15@gmail.com>
2026-03-21 20:29:46 -0400

    sticky Navbar

--- ecommerce-app/src/components/Navbar.jsx
+++ ecommerce-app/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-900 text-white px-6 py-4 flex gap-6 shadow-md flex justify-between items-center">
+    <nav className="sticky top-0 z-50 bg-gray-900 text-white px-6 py-4 flex gap-6 shadow-md flex justify-between items-center">
       {/* Left side: Logo or title */}
       <Link to="/products" className="text-2xl font-bold text-white-800">
         My Store
--- ecommerce-app/src/components/ProductList.jsx
+++ ecommerce-app/src/components/ProductList.jsx
@@ -13,14 +13,14 @@ export default function ProductList({ products }) {
       <h1 className="text-3xl font-bold mb-4"></h1>
 
       <div className="flex justify-center w-full">
-  <input
-    type="text"
-    placeholder="Search products..."
-    value={search}
-    onChange={(e) => setSearch(e.target.value)}
-    className="border p-2 w-80 mb-6 rounded shadow-sm focus:ring-2 focus:ring-black outline-none transition-all"
-  />
-</div>
+        <input
+        type="text"
+        placeholder="Search products..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="border p-2 w-80 mb-6 rounded shadow-sm focus:ring-2 focus:ring-black outline-none transition-all"
+        />
+      </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {filteredProducts.map((p) => (
@@ -28,27 +28,25 @@ export default function ProductList({ products }) {
             key={p.id}
             className="bg-white border border-gray-100 border rounded-lg p-4 shadow hover:shadow-lg transition"
           >
-             <div>
+            <div>
                   <img
                     src={p.image}
                     alt={p.name}
                     className="w-full h-40 object-cover rounded"
                   />
-            <h3 className="text-xl font-semibold mt-2">{p.name}</h3>
-            <p className="text-gray-600">${p.price}</p>
+                <h3 className="text-xl font-semibold mt-2">{p.name}</h3>
+                <p className="text-gray-600">${p.price}</p>
 
-            <Link
-              to={`/products/${p.id}`}
-              className="inline-block mt-3 text-blue-600 hover:underline"
-            >
-              View Details
-            </Link>
+                <Link
+                to={`/products/${p.id}`}
+                className="inline-block mt-3 text-blue-600 hover:underline"
+                >
+                    View Details
+                </Link>
+            </div>
           </div>
-        </div>
-       
         ))}
       </div>
-      
     </div>
   );
 }
